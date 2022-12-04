fe9aa4e71faef9c5f8325787c20ca369498b1972
TimzOwen <timzowen@gmail.com>
2022-12-04 22:20:53 +0300

    alignment of views

--- androidlogo/app/src/main/java/com/timzowen/androidlogo/MainActivity.kt
+++ androidlogo/app/src/main/java/com/timzowen/androidlogo/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,28 +47,46 @@ fun ComposableAndroidLogo() {
         Image(
             painter = painterResource(id = R.drawable.android_logo),
             contentDescription = "Android logo",
+            contentScale = ContentScale.Crop,
             modifier = Modifier
-                .wrapContentWidth()
-                .wrapContentHeight()
-        )
-        Text(
-            text = "Testing centered text"
-        )
-        Text(text = "One more Text Centered"
-        )
-        Text(text = "3rd text aligned again"
+                .width(100.dp)
+                .height(100.dp)
+                .clip(CircleShape)
+
         )
     }
     Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Row{
+        Row(horizontalArrangement = Arrangement.Center){
             Image(
                 painter = painterResource(id = R.drawable.ic_phone) ,
+                modifier = Modifier.padding(30.dp),
                 contentDescription ="phone" )
             Text(
-                text = "+254740254637"
+                text = "+254740254637",
+                modifier = Modifier.padding(start = 30.dp, end = 30.dp, bottom = 15.dp),
+            )
+        }
+        Row(horizontalArrangement = Arrangement.Center){
+            Image(
+                painter = painterResource(id = R.drawable.ic_phone) ,
+                modifier = Modifier.padding(start = 30.dp, end = 30.dp, bottom = 15.dp),
+                contentDescription ="phone" )
+            Text(
+                text = "+254740254637",
+                modifier = Modifier.padding(30.dp),
+            )
+        }
+        Row(horizontalArrangement = Arrangement.Center){
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_contact_mail_24) ,
+                modifier = Modifier.padding(start = 30.dp, end = 30.dp, bottom = 30.dp),
+                contentDescription ="phone" )
+            Text(
+                modifier = Modifier.padding(30.dp),
+                text = "timz.owen@google.com"
             )
         }
 
